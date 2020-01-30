d20cfcaa47569adb35e9c717552e4eceface90e9
kleber de Souza <klsdesigner@hotmail.com>
2020-01-30 20:45:59 -0300

    Criando Model e migration para Banner

---
+++ database/migrations/2020_01_30_213716_create_banners_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->string('link')->nullable();
+            $table->integer('posicao');
+            $table->integer('ordem');
+            $table->$table->enum('status', ['A', 'I'])->default(['A']);
+            $table->string('imagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
