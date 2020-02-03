2ec4f5f400c6d0ba2d52e4bbb9b92791be5006ef
kleber de Souza <klsdesigner@hotmail.com>
2020-02-03 20:24:18 -0300

    Criado a model e a migration para depoimento

---
+++ database/migrations/2020_02_03_211603_create_depoimentos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepoimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('depoimentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->longText('depoimento');
+            $table->enum('status', ['A', 'I'])->nullable()->default('A')->comment('Ativo ou Inativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('depoimentos');
+    }
+}
